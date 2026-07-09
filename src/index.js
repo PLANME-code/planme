51d31f8a01040957d7f889977b51a99b38dd6b0c
PLANME-code <nafissa.tizaoui@hotmail.com>
2026-07-09 16:53:21 +0200

    Change import statement to use app.jsx

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import app from './app';
+import app from './app.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<React.StrictMode><App /></React.StrictMode>);
