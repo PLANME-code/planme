4c37a74c8a75bf7d980128cfef6b273a03be6115
PLANME-code <nafissa.tizaoui@hotmail.com>
2026-07-09 17:05:25 +0200

    Rename import from app.jsx to App

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import app from './app.jsx';
+import app from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<React.StrictMode><App /></React.StrictMode>);
